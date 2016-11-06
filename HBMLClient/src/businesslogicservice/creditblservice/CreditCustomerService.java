0aa17aa209c69523311bedddd5b34ac3c1d35abd
KingOfSolo <944125893@qq.com>
2016-11-06 20:46:47 +0800

    Credit接口

---
+++ HBMLClient/src/businesslogicservice/creditblservice/CreditCustomerService.java
@@ -0,0 +1,7 @@
+package businesslogicservice.creditblservice;
+
+public interface CreditCustomerService {
+	public String getChangeType(String id);
+	public String getReasonType(String id);
+	public String getAccount(String id);
+}
