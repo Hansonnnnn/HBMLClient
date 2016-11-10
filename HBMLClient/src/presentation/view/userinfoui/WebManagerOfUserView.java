ad01bd2fd60a90146cebb6aecab2f297e547e338
AlexYu-beta <yym1996@gmail.com>
2016-11-10 10:41:02 +0800

    ver0.2 @11/10/2016 presentation(controller, view) //task to be done: refinement

---
+++ HBMLClient/src/presentation/view/userinfoui/WebManagerOfUserView.java
@@ -0,0 +1,44 @@
+package presentation.view.userinfoui;
+
+import vo.WebMarketerVO;
+
+import javax.swing.*;
+
+/**
+ * Created by alex on 16-11-10.
+ */
+public class WebManagerOfUserView extends JPanel{
+    private static final long serialVersionUID = 1L;
+    private UserInfoViewControllerService controller;
+    public WebManagerOfUserView(UserInfoViewControllerService userInfoViewControllerService){
+        this.controller=userInfoViewControllerService;
+    }
+
+    public void showUser(){
+        controller.showUserInfo();
+    }
+
+    public void showUserList(){
+        controller.showUserInfoList();
+    }
+
+    public void login(String id,String pwd){
+        controller.login(id,pwd);
+    }
+
+    public void logout(){
+        controller.logout();
+    }
+
+    public void modifyUserInfo(){
+        controller.modifyUserInfo(new WebMarketerVO("00020"));
+    }
+
+    public void addUserInfo(){
+        controller.addUserInfo(new WebMarketerVO("00020"));
+    }
+
+    public void deleteUserInfo(){
+        controller.deleteUserInfo(new WebMarketerVO("00020"));
+    }
+}
