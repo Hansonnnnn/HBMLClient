eb9f237c74fa0ac3bada39e2b3c5725181d352fc
649433713 <649433713@qq.com>
2016-12-01 19:08:35 +0800

    更新po包

--- HBMLClient/src/main/java/dao/hotel/HotelDao.java
+++ HBMLClient/src/main/java/dao/hotel/HotelDao.java
@@ -2,22 +2,30 @@ package dao.hotel;
 
 import java.rmi.Remote;
 import java.rmi.RemoteException;
+import java.util.List;
 import java.util.Map;
 
 import message.ResultMessage;
 import model.HotelFilter;
+import po.CommentInfoPO;
 import po.HotelPO;
+import po.RegionPO;
 
 public interface HotelDao extends Remote{
 	
 	public Map<Integer, HotelPO> getHotelList(HotelFilter filter)throws RemoteException;	
 	
-	public HotelPO getHotelInfo(String hotel_ID)throws RemoteException;
+	public HotelPO getHotelInfo(int hotelID)throws RemoteException;
 	
 	public ResultMessage addHotel(HotelPO po)throws RemoteException;
 	
 	public ResultMessage modifyHotel(HotelPO po)throws RemoteException;
 	
 	public ResultMessage deleteHotel(int hotel_ID)throws RemoteException;
-	
+
+	public List<CommentInfoPO> getComments(int hotelID)throws RemoteException;
+
+	public ResultMessage addComment(CommentInfoPO commentInfoPO)throws RemoteException;
+
+	public Map<Integer, RegionPO> getRegions()throws RemoteException;
 }
--- HBMLClient/src/main/java/po/RoomInfoPO.java
+++ HBMLClient/src/main/java/po/RoomInfoPO.java
@@ -2,55 +2,88 @@ package po;
 
 
 import java.io.Serializable;
-import java.util.List;
+import java.util.Date;
 
 public class RoomInfoPO implements Serializable{
-	List<RoomInfoPO> roomList;
+	int roomInfoID;
+	int hotelID;
+	String roomID;
 	String roomType;
-	int price;
+	int roomPrice;
+	int roomState;
+	Date detailedInfo1;
+	Date detailedInfo2;
+	public RoomInfoPO(int roomInfoID, int hotelID, String roomID, String roomType, int roomPrice, int roomState,
+			Date detailedInfo1, Date detailedInfo2) {
+		super();
+		this.roomInfoID = roomInfoID;
+		this.hotelID = hotelID;
+		this.roomID = roomID;
+		this.roomType = roomType;
+		this.roomPrice = roomPrice;
+		this.roomState = roomState;
+		this.detailedInfo1 = detailedInfo1;
+		this.detailedInfo2 = detailedInfo2;
+	}
 	
 	
-	public RoomInfoPO(List<RoomInfoPO> rL,String rT,int p){
-		roomList=rL;
-		roomType=rT;
-		price=p;
-		
+	@Override
+	public String toString() {
+		return "RoomInfoPO [roomInfoID=" + roomInfoID + ", hotelID=" + hotelID + ", roomID=" + roomID + ", roomType="
+				+ roomType + ", roomPrice=" + roomPrice + ", roomState=" + roomState + ", detailedInfo1="
+				+ detailedInfo1 + ", detailedInfo2=" + detailedInfo2 + "]";
 	}
 
 
-	public RoomInfoPO() {
-		// TODO Auto-generated constructor stub
+	public int getRoomInfoID() {
+		return roomInfoID;
 	}
-
-
-	public List<RoomInfoPO> getRoomList() {
-		return roomList;
+	public void setRoomInfoID(int roomInfoID) {
+		this.roomInfoID = roomInfoID;
 	}
-
-
-	public void setRoomList(List<RoomInfoPO> roomList) {
-		this.roomList = roomList;
+	public int getHotelID() {
+		return hotelID;
 	}
-
-
+	public void setHotelID(int hotelID) {
+		this.hotelID = hotelID;
+	}
+	public String getRoomID() {
+		return roomID;
+	}
+	public void setRoomID(String roomID) {
+		this.roomID = roomID;
+	}
 	public String getRoomType() {
 		return roomType;
 	}
-
-
 	public void setRoomType(String roomType) {
 		this.roomType = roomType;
 	}
-
-
-	public int getPrice() {
-		return price;
+	public int getRoomPrice() {
+		return roomPrice;
 	}
-
-
-	public void setPrice(int price) {
-		this.price = price;
+	public void setRoomPrice(int roomPrice) {
+		this.roomPrice = roomPrice;
 	}
-
+	public int getRoomState() {
+		return roomState;
+	}
+	public void setRoomState(int roomState) {
+		this.roomState = roomState;
+	}
+	public Date getDetailedInfo1() {
+		return detailedInfo1;
+	}
+	public void setDetailedInfo1(Date detailedInfo1) {
+		this.detailedInfo1 = detailedInfo1;
+	}
+	public Date getDetailedInfo2() {
+		return detailedInfo2;
+	}
+	public void setDetailedInfo2(Date detailedInfo2) {
+		this.detailedInfo2 = detailedInfo2;
+	}
+	
+	
 	
 }
