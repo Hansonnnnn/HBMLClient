f61421522bb15078aa5641e3365e9f88e0da2c4a
KingOfSolo <944125893@qq.com>
2016-12-20 22:34:40 +0800

    界面对接2.9

--- HBMLClient/src/main/java/presentation/view/application/WorkerLoginUIController.java
+++ HBMLClient/src/main/java/presentation/view/application/WorkerLoginUIController.java
@@ -1,8 +1,10 @@
 package presentation.view.application;
 
+import businesslogic.userbl.UserLogImpl;
 import businesslogic.userbl.UserStaffImpl;
 import businesslogic.userbl.UserWebManagerImpl;
 import businesslogic.userbl.UserWebMarketerImpl;
+import businesslogicservice.userblservice.UserLogService;
 import businesslogicservice.userblservice.UserStaffService;
 import businesslogicservice.userblservice.UserWebManagerService;
 import businesslogicservice.userblservice.UserWebMarketerService;
@@ -30,15 +32,17 @@ public class WorkerLoginUIController {
 
     private Stage stage;
     private Scene thisScene;
-    private UserStaffService userStaffService;
-    private UserWebManagerService userWebManagerService;
-    private UserWebMarketerService webMarketerService;
+//    private UserStaffService userStaffService;
+//    private UserWebManagerService userWebManagerService;
+//    private UserWebMarketerService webMarketerService;
+    private UserLogService userLogService;
     public void init(Stage stage,Scene thisScene){
         this.stage=stage;
         this.thisScene=thisScene;
-        userStaffService=new UserStaffImpl();
-        userWebManagerService=new UserWebManagerImpl();
-        webMarketerService=new UserWebMarketerImpl();
+//        userStaffService=new UserStaffImpl();
+//        userWebManagerService=new UserWebManagerImpl();
+//        webMarketerService=new UserWebMarketerImpl();
+        userLogService=new UserLogImpl();
         workerAccountTextField.setText("01");
         workerPasswordTextField.setText("admin");
     }
@@ -59,26 +63,26 @@ public class WorkerLoginUIController {
         try{
 ////            ResultMessage message=webMarketerService.login(workerAccountTextField.getText(),workerPasswordTextField.getText());
 //            ResultMessage message=userWebManagerService.login(workerAccountTextField.getText(),workerPasswordTextField.getText());
-////            ResultMessage message=userStaffService.login(workerAccountTextField.getText(),workerPasswordTextField.getText());
-//            System.out.println(message);
-//            if(message.equals(ResultMessage.wrongPassword)){
-//                new MyDialog(stage,"密码错误，请重新输入",0);
-//            }else if(message.equals(ResultMessage.notexist)){
-//                new MyDialog(stage,"该用户不存在",0);
-//            }else if(message.equals(ResultMessage.success)){
-//                UserVO userVO=userStaffService.getUserData(workerAccountTextField.getText());
-//                System.out.println("  aa  "+userVO.getUserType());
-//                if(userVO==null){
-//                    System.out.println("userVO为空");
-//                }
-//                if(userVO.getUserType().equals(UserType.Staff)){
-//                    stage.setScene(new HotelStaffUI(new Group(),stage,thisScene,userVO));
-//                }else if(userVO.getUserType().equals(UserType.WebManager)){
-//                    stage.setScene(new WebManagerUI(new Group(),stage,thisScene,userVO));
-//                }else if(userVO.getUserType().equals(UserType.WebMarketer)){
-//                    stage.setScene(new WebMarketerUI(new Group(),stage,thisScene,userVO));
-//                }
-//            }
+            ResultMessage message=userLogService.login(workerAccountTextField.getText(),workerPasswordTextField.getText());
+            System.out.println(message);
+            if(message.equals(ResultMessage.wrongPassword)){
+                new MyDialog(stage,"密码错误，请重新输入",0);
+            }else if(message.equals(ResultMessage.notexist)){
+                new MyDialog(stage,"该用户不存在",0);
+            }else if(message.equals(ResultMessage.success)){
+                UserVO userVO=userLogService.getUserData(workerAccountTextField.getText());
+                System.out.println("  aa  "+userVO.getUserType());
+                if(userVO==null){
+                    System.out.println("userVO为空");
+                }
+                if(userVO.getUserType().equals(UserType.Staff)){
+                    stage.setScene(new HotelStaffUI(new Group(),stage,thisScene,userVO));
+                }else if(userVO.getUserType().equals(UserType.WebManager)){
+                    stage.setScene(new WebManagerUI(new Group(),stage,thisScene,userVO));
+                }else if(userVO.getUserType().equals(UserType.WebMarketer)){
+                    stage.setScene(new WebMarketerUI(new Group(),stage,thisScene,userVO));
+                }
+            }
 //            stage.setScene(new WebMarketerUI(new Group(),stage,thisScene));
 //            stage.setScene(new HotelStaffUI(new Group(),stage,thisScene));
         }catch(Exception e){
