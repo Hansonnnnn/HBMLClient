d147e7f37ffc65dd66c89efda106f4ed1f42c8ea
649433713 <649433713@qq.com>
2016-12-20 22:53:39 +0800

    补commit

---
+++ HBMLClient/src/main/java/businesslogic/userbl/UserLogImpl.java
@@ -0,0 +1,60 @@
+package businesslogic.userbl;
+
+import businesslogic.userbl.helper.UserHelper;
+import businesslogicservice.userblservice.UserLogService;
+import message.ResultMessage;
+import model.UserType;
+import vo.UserVO;
+
+/**
+ * Created by alex on 12/20/16.
+ */
+public class UserLogImpl implements UserLogService{
+
+    UserHelper userHelper;
+
+    public UserLogImpl(){
+        userHelper=new UserHelper();
+    }
+    @Override
+    public ResultMessage login(String accountName, String pwd) throws Exception {
+
+        return userHelper.login(accountName,pwd);
+
+    }
+
+    @Override
+    public ResultMessage signup(UserVO vo) throws Exception {
+
+        ResultMessage resultMessage=userHelper.signup(vo);
+        if(resultMessage.equals(ResultMessage.success)){
+            System.out.println("successfully signing up a user!");
+        }
+        return resultMessage;
+
+    }
+
+    @Override
+    public ResultMessage logout(String accountName) throws Exception {
+
+        return userHelper.logout(accountName);
+
+    }
+
+    @Override
+    public UserVO getUserData(int userID) throws Exception{
+
+        UserVO userVO=userHelper.getUserData(userID);
+
+        return userVO;
+
+    }
+
+    @Override
+    public UserVO getUserData(String accountName) throws Exception{
+
+        UserVO userVO=userHelper.getUserData(accountName);
+        return userVO;
+
+    }
+}
