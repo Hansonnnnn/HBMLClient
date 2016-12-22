a942129fa071d8e13f32b87be99835869f7469e6
RadiationXie <xiezhenyu@192.168.1.104>
2016-12-23 00:27:04 +0800

    谢振宇端最新版

--- HBMLClient/src/main/java/presentation/view/customerui/customerui/PersonInfoPageController.java
+++ HBMLClient/src/main/java/presentation/view/customerui/customerui/PersonInfoPageController.java
@@ -5,6 +5,7 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 
+import businesslogic.userbl.UserCustomerImpl;
 import businesslogicservice.userblservice.UserCustomerService;
 import javafx.fxml.FXML;
 import javafx.scene.Group;
@@ -17,6 +18,7 @@ import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.stage.FileChooser;
 import javafx.stage.Stage;
+import presentation.view.application.MyDialog;
 import vo.UserVO;
 
 public class PersonInfoPageController {
@@ -29,6 +31,7 @@ public class PersonInfoPageController {
 	@FXML private DatePicker yearPicker;
 	@FXML private TextField numberField;
 	@FXML private TextField infoField;
+	@FXML private Button saveButton;
 	
 	private Stage stage;
 	private Scene preScene;
@@ -46,6 +49,7 @@ public class PersonInfoPageController {
 		this.preScene = preScene;
 		this.presentScene = presentScene;
 		this.userVO = userVO;
+		customerService = new UserCustomerImpl();
 		initHeadImage();
 	}
 	
@@ -82,39 +86,43 @@ public class PersonInfoPageController {
 				);
 		if (choiceFile!=null) 
 		{
-			 try{
-	                String path="/Users/xiezhenyu/Desktop/Pic";
-	                String fileName=path+choiceFile.getName().toString();
-	                File file=new File(fileName);
-	                if(!file.exists()){
-	                    File newfile=new File(path);
-	                    newfile.mkdirs();
-	                    FileInputStream input=new FileInputStream(choiceFile);
-	                    FileOutputStream output=new FileOutputStream(fileName);
-
-	                    byte[] b=new byte[1824*5];
-	                    int length;
-	                    while((length=input.read(b))!=-1){
-	                        output.write(b,0,length);
-	                    }
-
-	                    output.flush();
-	                    output.close();
-	                    input.close();
-	                }
-	                Image image=new Image("file:///"+fileName);
-	                
+//			 try{
+//	                String path="/Users/xiezhenyu/Desktop/Pic";
+//	                String fileName=path+choiceFile.getName().toString();
+//	                File file=new File(fileName);
+//	                if(!file.exists()){
+//	                    File newfile=new File(path);
+//	                    newfile.mkdirs();
+//	                    FileInputStream input=new FileInputStream(choiceFile);
+//	                    FileOutputStream output=new FileOutputStream(fileName);
+//
+//	                    byte[] b=new byte[1824*5];
+//	                    int length;
+//	                    while((length=input.read(b))!=-1){
+//	                        output.write(b,0,length);
+//	                    }
+//
+//	                    output.flush();
+//	                    output.close();
+//	                    input.close();
+//	                }
+	                Image image=new Image("file:///"+choiceFile.getPath());
 	                headImageView.setImage(image);
-
-	            }catch (Exception e){
-	                e.printStackTrace();
-	            }
+//	            }catch (Exception e){
+//	                e.printStackTrace();
+//	            }
 		}
 	}
 	
 	@FXML
 	private void save()
 	{
-		
+		userVO = new UserVO(userVO.getUserID(), userVO.getUserType(),
+				userVO.getAccountName(), userVO.getPassword(), userVO.getName(), 
+				userVO.getContact(), choiceFile, userVO.getCreditValue(),
+				userVO.getMemberType(), userVO.getMemberInfo(), userVO.getRank(),
+				userVO.getWorkid(), userVO.getHotelid());
+		customerService.modifyUser(userVO);
+		new MyDialog(stage, "保存信息成功", 2);
 	}
 }
