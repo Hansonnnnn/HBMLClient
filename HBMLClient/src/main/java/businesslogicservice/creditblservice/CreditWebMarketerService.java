60ab9ed57f1c43a793928356d924108305e793bc
alex2097 <151250187@smail.nju.edu.cn>
2017-01-01 19:55:36 +0800

    commit 0.4.1 modify promotionVO add some comments

--- HBMLClient/src/main/java/businesslogicservice/creditblservice/CreditWebMarketerService.java
+++ HBMLClient/src/main/java/businesslogicservice/creditblservice/CreditWebMarketerService.java
@@ -11,21 +11,28 @@ import vo.RankVO;
  */
 public interface CreditWebMarketerService {
     /**
-     * 恢复全部，输入type==1,恢复一半，输入type==0
-     *
+     * 恢复信用值
+	 * 恢复全部，输入type==1,恢复一半，输入type==0
      * @param userID
      * @param
      * @return
      */
     public ResultMessage resumeCreditValue(int userID,long price,int type,int orderID);
 
+
+	/**
+	 * 增加信用值（通用方法，不给用户使用）
+	 * @param UserID
+	 * @param value
+	 * @return
+	 */
     public ResultMessage addCreditValue(int UserID,long value);
     
     
     
     
     /**
-     * @TODO：得到当前的等级制度  RankVO里三个参数  rank表示等级，discount表示折扣。value表示达到该等级所需信用值
+     * 得到当前的等级制度  RankVO里三个参数  rank表示等级，discount表示折扣。value表示达到该等级所需信用值
      * @return  Map的key  （integer）为 RankVO.rank
      */
     public Map<Integer, RankVO> getRankList() ;
@@ -33,7 +40,7 @@ public interface CreditWebMarketerService {
     
     
 	/**
-	 * @TODO： 修改等级规则
+	 * 修改等级规则
 	 * @param newRule  这个Map就是这个Map  里面的对象就这么多不能变   改哪个等级 用newRule.get(key) 方法得到
 	 * 			RankVO修改 
 	 * @return
