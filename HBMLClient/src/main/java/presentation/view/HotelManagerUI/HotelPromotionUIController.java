a116b25bcd63227e5e730b8020cc3ed17b9097fd
KingOfSolo <944125893@qq.com>
2017-01-02 11:33:04 +0800

    心态爆炸后的浴火重生

--- HBMLClient/src/main/java/presentation/view/HotelManagerUI/HotelPromotionUIController.java
+++ HBMLClient/src/main/java/presentation/view/HotelManagerUI/HotelPromotionUIController.java
@@ -9,8 +9,6 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.Group;
-import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.image.Image;
@@ -29,16 +27,26 @@ import vo.UserVO;
  */
 public class HotelPromotionUIController {
 
-    @FXML private TableView hotelPromotionTableView;
-    @FXML private TableColumn promotionNameTableColumn;
-    @FXML private TableColumn promotionStartTableColumn;
-    @FXML private TableColumn promotionEndTableColumn;
-    @FXML private TableColumn operationTableColumn;
-    @FXML private ComboBox customerDiscountBox;
-    @FXML private ComboBox companyDiscountBox;
-    @FXML private VBox hotelPromotionVBox;
-    @FXML private VBox otherPromotionVBox1;
-    @FXML private Label sliderLabel;
+    @FXML
+    private TableView hotelPromotionTableView;
+    @FXML
+    private TableColumn promotionNameTableColumn;
+    @FXML
+    private TableColumn promotionStartTableColumn;
+    @FXML
+    private TableColumn promotionEndTableColumn;
+    @FXML
+    private TableColumn operationTableColumn;
+    @FXML
+    private ComboBox<Integer> customerDiscountBox;
+    @FXML
+    private ComboBox<Integer> companyDiscountBox;
+    @FXML
+    private VBox hotelPromotionVBox;
+    @FXML
+    private VBox otherPromotionVBox1;
+    @FXML
+    private Label sliderLabel;
 
     private ObservableList promotionData;
     private VBox infoVBox;
@@ -46,37 +54,41 @@ public class HotelPromotionUIController {
     private Stage stage;
     private UserVO userVO;
     private PromotionStaffService promotionStaffService;
-    public void init(VBox infoVBox, VBox thisVBox, Stage stage, UserVO userVO){
-        this.infoVBox=infoVBox;
-        this.thisVBox=thisVBox;
-        this.stage=stage;
-        this.userVO=userVO;
-        promotionStaffService=new PromotionStaffImpl();
+    private ObservableList discountData;
+
+    public void init(VBox infoVBox, VBox thisVBox, Stage stage, UserVO userVO) {
+        this.infoVBox = infoVBox;
+        this.thisVBox = thisVBox;
+        this.stage = stage;
+        this.userVO = userVO;
+        promotionStaffService = new PromotionStaffImpl();
         initTableView();
     }
 
     /**
      * 初始化hotelPromotionTableView
      */
-    private void initTableView(){
+    private void initTableView() {
         promotionNameTableColumn.setCellValueFactory(new PropertyValueFactory<>("name"));
         promotionStartTableColumn.setCellValueFactory(new PropertyValueFactory<>("startDateString"));
         promotionEndTableColumn.setCellValueFactory(new PropertyValueFactory<>("endDateString"));
-        operationTableColumn.setCellFactory(new Callback<TableColumn<PromotionVO,Boolean>, TableCell<PromotionVO,Boolean>>() {
+        operationTableColumn.setCellFactory(new Callback<TableColumn<PromotionVO, Boolean>, TableCell<PromotionVO, Boolean>>() {
             @Override
             public TableCell call(TableColumn param) {
-                return new PromotionInfoButtonCell(infoVBox,thisVBox);
+                return new PromotionInfoButtonCell(infoVBox, thisVBox);
             }
         });
 
-        promotionData=FXCollections.observableArrayList();
-        PromotionFilter promotionFilter=new PromotionFilter();
-        promotionFilter.add("region","=",userVO.getHotelid());
-        if(promotionStaffService.getHotelPromotionList(promotionFilter)!=null){
-            for (PromotionVO promotionVO:promotionStaffService.getHotelPromotionList(promotionFilter).values()) {
-                promotionData.add(promotionVO);
+        promotionData = FXCollections.observableArrayList();
+        PromotionFilter promotionFilter = new PromotionFilter();
+        promotionFilter.add("region", "=", userVO.getHotelid());
+        if (promotionStaffService.getHotelPromotionList(promotionFilter) != null) {
+            for (PromotionVO promotionVO : promotionStaffService.getHotelPromotionList(promotionFilter).values()) {
+                if ((!promotionVO.getName().equals("00")) && (!promotionVO.getName().equals("01"))) {
+                    promotionData.add(promotionVO);
+                }
             }
-        }else{
+        } else {
             System.out.println("数据库促销数据为空");
         }
         hotelPromotionTableView.setItems(promotionData);
@@ -86,30 +98,111 @@ public class HotelPromotionUIController {
      * 点击酒店活动优惠按钮,显示优惠策略列表
      */
     @FXML
-    private void toHotelPromotion(){
+    private void toHotelPromotion() {
         hotelPromotionVBox.setVisible(true);
         otherPromotionVBox1.setVisible(false);
-        moveOperationButtonBackground(sliderLabel,35);
+        moveOperationButtonBackground(sliderLabel, 35);
     }
 
     /**
      * 点击其他促销策略按钮，显示生日和企业优惠界面
      */
     @FXML
-    private void toOtherPromotion(){
+    private void toOtherPromotion() {
+        discountData = FXCollections.observableArrayList();
+        for (int i = 1; i < 100; i++) {
+            discountData.add(i);
+        }
+        customerDiscountBox.setItems(discountData);
+        companyDiscountBox.setItems(discountData);
+        PromotionFilter customerPromotionFilter = new PromotionFilter();
+        customerPromotionFilter.add("region", "=", userVO.getHotelid());
+        if (promotionStaffService.getHotelPromotionList(customerPromotionFilter) != null) {
+            for (PromotionVO promotionVO : promotionStaffService.getHotelPromotionList(customerPromotionFilter).values()) {
+                if (promotionVO.getName().equals("00")) {
+                    customerDiscountBox.setValue(promotionVO.getDiscount());
+                }
+            }
+        }
+
+        PromotionFilter companyPromotionFilter = new PromotionFilter();
+        companyPromotionFilter.add("region", "=", userVO.getHotelid());
+//        companyPromotionFilter.add("name","=","01");
+        if (promotionStaffService.getHotelPromotionList(companyPromotionFilter) != null) {
+            for (PromotionVO promotionVO : promotionStaffService.getHotelPromotionList(companyPromotionFilter).values()) {
+                if (promotionVO.getName().equals("01")) {
+                    companyDiscountBox.setValue(promotionVO.getDiscount());
+                }
+            }
+        }
         otherPromotionVBox1.setVisible(true);
         hotelPromotionVBox.setVisible(false);
-        moveOperationButtonBackground(sliderLabel,165);
+        moveOperationButtonBackground(sliderLabel, 165);
     }
+
+    /**
+     * 点击编辑按钮，编辑会员生日优惠和企业优惠   
+     */
+    @FXML
+    private void edit() {
+        customerDiscountBox.setDisable(false);
+        companyDiscountBox.setDisable(false);
+    }
+
+    /**
+     * 点击确认修改按钮，保存会员生日优惠和企业优惠
+     */
+    @FXML
+    private void confirm() {
+        customerDiscountBox.setDisable(true);
+        companyDiscountBox.setDisable(true);
+        PromotionFilter customerPromotionFilter = new PromotionFilter();
+        customerPromotionFilter.add("region", "=", userVO.getHotelid());
+        //如果该酒店的会员生日优惠已经制定过，那么就进行修改。如果没有制定过，就添加新的促销策略
+        if (promotionStaffService.getHotelPromotionList(customerPromotionFilter) != null) {
+            PromotionVO tempPromotionVO = null;
+            for (PromotionVO promotionVO : promotionStaffService.getHotelPromotionList(customerPromotionFilter).values()) {
+                if (promotionVO.getName().equals("00")) {
+                    tempPromotionVO = promotionVO;
+                }
+            }
+            if (tempPromotionVO != null) {
+                PromotionVO promotionVO = new PromotionVO(userVO.getHotelid(), customerDiscountBox.getValue(), 0, tempPromotionVO.getPromotionID(), 0);
+                promotionStaffService.modifyHotelPromotion(promotionVO);
+            } else {
+                PromotionVO newPromotionVO = new PromotionVO(userVO.getHotelid(), customerDiscountBox.getValue(), 0);
+                promotionStaffService.addHotelPromotion(newPromotionVO);
+            }
+        }
+        //如果该酒店的企业优惠已经制定过，那么就进行修改。如果没有制定过，就添加新的促销策略
+        PromotionFilter companyPromotionFilter = new PromotionFilter();
+        companyPromotionFilter.add("region", "=", userVO.getHotelid());
+        if (promotionStaffService.getHotelPromotionList(companyPromotionFilter) != null) {
+            PromotionVO tempPromotionVO = null;
+            for (PromotionVO promotionVO : promotionStaffService.getHotelPromotionList(companyPromotionFilter).values()) {
+                if (promotionVO.getName().equals("01")) {
+                    tempPromotionVO = promotionVO;
+                }
+            }
+            if (tempPromotionVO != null) {
+                PromotionVO promotionVO = new PromotionVO(userVO.getHotelid(), companyDiscountBox.getValue(), 1, tempPromotionVO.getPromotionID(), 0);
+                promotionStaffService.modifyHotelPromotion(promotionVO);
+            } else {
+                PromotionVO newPromotionVO = new PromotionVO(userVO.getHotelid(), companyDiscountBox.getValue(), 1);
+                promotionStaffService.addHotelPromotion(newPromotionVO);
+            }
+        }
+    }
+
 
     /**
      * 设置按钮下边界滑动效果
      */
-    private void moveOperationButtonBackground(Label label, double x){
-        Timeline timeline=new Timeline();
+    private void moveOperationButtonBackground(Label label, double x) {
+        Timeline timeline = new Timeline();
         timeline.setAutoReverse(false);
-        KeyValue newX=new KeyValue(label.layoutXProperty(),x);
-        KeyFrame kf=new KeyFrame(Duration.millis(300),newX);
+        KeyValue newX = new KeyValue(label.layoutXProperty(), x);
+        KeyFrame kf = new KeyFrame(Duration.millis(300), newX);
         timeline.getKeyFrames().add(kf);
         timeline.play();
     }
@@ -119,41 +212,42 @@ public class HotelPromotionUIController {
      * 跳转至制定新促销策略界面
      */
     @FXML
-    private void toNewPromotion(){
+    private void toNewPromotion() {
         infoVBox.getChildren().remove(0);
-        infoVBox.getChildren().add(new NewPromotionUI(infoVBox,thisVBox,userVO,stage));
+        infoVBox.getChildren().add(new NewPromotionUI(infoVBox, thisVBox, userVO, stage));
     }
 
     /**
      * 在酒店促销策略列表添加查看策略信息按钮
      */
-    public class PromotionInfoButtonCell extends TableCell<PromotionVO,Boolean>{
-        private HBox operationHBox=new HBox();
-        private Button deleteButton=new Button();
-        private Button viewButton=new Button();
-        private ImageView viewImageView=new ImageView(new Image(getClass().getResourceAsStream("ManagerImages/view1.png")));
-        private ImageView deleteImageView=new ImageView(new Image(getClass().getResourceAsStream("ManagerImages/delete.png")));
-        public PromotionInfoButtonCell(VBox infoVBox,VBox beforeVBox){
-            operationHBox.getChildren().addAll(viewButton,deleteButton);
+    public class PromotionInfoButtonCell extends TableCell<PromotionVO, Boolean> {
+        private HBox operationHBox = new HBox();
+        private Button deleteButton = new Button();
+        private Button viewButton = new Button();
+        private ImageView viewImageView = new ImageView(new Image(getClass().getResourceAsStream("ManagerImages/view1.png")));
+        private ImageView deleteImageView = new ImageView(new Image(getClass().getResourceAsStream("ManagerImages/delete.png")));
+
+        public PromotionInfoButtonCell(VBox infoVBox, VBox beforeVBox) {
+            operationHBox.getChildren().addAll(viewButton, deleteButton);
             viewButton.setGraphic(viewImageView);
             viewButton.setStyle("-fx-background-color: transparent");
             deleteButton.setGraphic(deleteImageView);
             deleteButton.setStyle("-fx-background-color: transparent");
-            viewButton.setOnAction((ActionEvent e)->{
-                int selectedIndex=getTableRow().getIndex();
-                PromotionVO promotionVO=(PromotionVO)hotelPromotionTableView.getItems().get(selectedIndex);
+            viewButton.setOnAction((ActionEvent e) -> {
+                int selectedIndex = getTableRow().getIndex();
+                PromotionVO promotionVO = (PromotionVO) hotelPromotionTableView.getItems().get(selectedIndex);
                 infoVBox.getChildren().remove(0);
-                infoVBox.getChildren().add(new PromotionInfoUI(infoVBox,beforeVBox,promotionVO));
+                infoVBox.getChildren().add(new PromotionInfoUI(infoVBox, beforeVBox, promotionVO));
             });
         }
 
         @Override
-        protected void updateItem(Boolean t,boolean empty){
-            super.updateItem(t,empty);
-            if(empty){
+        protected void updateItem(Boolean t, boolean empty) {
+            super.updateItem(t, empty);
+            if (empty) {
                 setGraphic(null);
                 setText(null);
-            }else{
+            } else {
                 setGraphic(operationHBox);
                 setText(null);
             }
